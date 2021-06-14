21223daf2557ed283281c817ee64b8fed7078d0c
braveheart-star <mouse1996115@gmail.com>
2021-06-14 17:01:41 +0800

    formatted

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,16 +6,12 @@ import { selectCreditCardData } from '../credit-card/credit-card.selector';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
   cardValue$ = this.store.pipe(select(selectCreditCardData));
 
-  constructor(private readonly store: Store<AppState>) { }
+  constructor(private readonly store: Store<AppState>) {}
 
-  ngOnInit(): void {
-  }
-
-
-
+  ngOnInit(): void {}
 }
